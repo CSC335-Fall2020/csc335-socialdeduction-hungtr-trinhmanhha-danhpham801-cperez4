50f384d93445394c7e2a10c34da767d934f65be4
Trinh Ha <trinhmanhha@email.arizona.edu>
2020-12-06 18:12:10 -0700

    Test committing to new branch

---
+++ src/GameView.java
@@ -0,0 +1,11 @@
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class GameView extends Scene {
+
+	public GameView(Parent arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
