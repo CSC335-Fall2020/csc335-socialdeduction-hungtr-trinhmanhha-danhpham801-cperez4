cd032a8ac3397549a1ca72665e431d6f8cf5208a
Phamda1340 <58050461+Phamda1340@users.noreply.github.com>
2020-12-07 12:07:43 -0700

    get model

--- src/GameModel.java
+++ src/GameModel.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * @author Hung Tran
@@ -9,38 +10,94 @@ import java.util.ArrayList;
  * File: GameModel.java
  * Desc: This file is a model class for the social deduction card-game
  */
-/**
- * Game description (Modified):
- * It's going to be a card game where the main objective is to reach the end
- * or find the saboteur. Each player is given 5 number cards at random and
- * the number will determine how much progress the group makes towards the
- * goal. There would be an event that pops up at a random time with a random
- * total value that the group will have to match or exceed by putting down
- * high number cards. 
- * 
- * [Strategy]
- * Generally, the saboteur will want to play lower cards
- * to delay the group from the goal; they, however, may gain trust by playing
- * high number cards in the beginning. There will be a time limit to how long
- * the game will last. The saboteur will have to survive till time runs out
- * or the good group is reduced to 1 or 2 players. 
- * 
- * [Mechanics]
- * It will play like a combination of UNO and Cards Against Humanity.
- * Event cards are different color then player cards.
- * Failure to meet the required amount in the event will somehow benefit the saboteur. {
- * 	Hung Tran: A murder card, maybe?
- * }
- * 
- * [Voting the saboteur]
- * Each player puts down 1 card each turn, each player has a pause button to debate 
- * who the saboteur is. The pause will have a global cooldown for every player 
- * in the game. 
- */
 public class GameModel {
-	int players; // The number of players
-	int progress; // The current progress of the gameboard, reach goal to win
-	int goal; // Self-explained
-	ArrayList<Integer> hands; // The current hand of player
-	int latest; // The latest card that has been played to the board
+	private Player[] players;
+	private Deck sharedDeck;
+	private int gameProgress;
+	private int eventSuccess;
+	private int eventFail;
+	private int traitor;
+	
+	
+	public GameModel(String[] playerNames) {
+		Random random = new Random();
+		int numPlayers = playerNames.length;
+		this.traitor = random.nextInt(numPlayers);
+		this.players = new Player[numPlayers];
+		this.sharedDeck = new Deck(numPlayers);
+		this.gameProgress = 5;
+		this.eventSuccess = 0;
+		this.eventFail = 0;
+		int i = 0;
+		for(String name: playerNames) {
+			if(traitor == i) {
+				this.players[i] = new Player(name, sharedDeck, true);
+			}else {
+				this.players[i] = new Player(name, sharedDeck);
+			}
+			i++;
+		}
+	}
+	
+	/*
+	 * eliminates a player form the game
+	 * returns 0 if the player was eliminated successfully, -1 if the player was already eliminated
+	 */
+	public int eliminate(String name) {
+		for(Player p: players) {
+			if(p.getName().toLowerCase().equals(name)) {
+				if(!p.isAlive()) {
+					return -1;
+				}
+				p.eliminate();
+			}
+		}
+		return 0;
+	}
+	
+	/*
+	 * checks to see if any of the end-game conditions have been met
+	 * returns -1 if the game is not over, 0 if the group wins, 1 if the saboteur wins
+	 */
+	public int isGameOver() {
+		//checks to see if the saboteur is voted out
+		if (!players[traitor].isAlive()) {
+			return 0;
+		//checks to see if there are no more events to be played
+		}else if(gameProgress == 0){
+			//if more events where successful then the group wins
+			if (eventSuccess > eventFail){
+				return 0;
+			//if more events fail, or if there is a tie then the saboteur wins
+			}else {
+				return 1;
+			}
+		}else {
+			//checks to see if there are only two players left (sabotuer and a +1)
+			int playersAlive = 0;
+			for(Player p : players) {
+				if (p.isAlive()) {
+					playersAlive++;
+				}
+			}
+			if(playersAlive == 2) {
+				return 1;
+			}
+		}
+		return -1;
+	}
+	
+	public String toString() {
+		String rep = "";
+		rep += "-Placeholder-\n";
+		rep += "Shared Deck:\n";
+		rep += this.sharedDeck.toString() + "\n\n";
+		rep += "Players:\n";
+		rep +="--------------------------------------------------------\n";
+		for(Player p: this.players) {
+			rep += p.toString() + "\n";
+			rep +="--------------------------------------------------------\n";
+		}
+		return rep;
+	}
 }
