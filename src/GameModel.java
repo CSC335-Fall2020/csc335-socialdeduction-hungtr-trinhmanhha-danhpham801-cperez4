a4d8157ddd0c2deb71cfb3386a1ce6090670a1ad
cesar <cesar@Shadow>
2020-12-06 21:39:59 -0700

    Update on the model
    game over conditions
    player elimination
    added

--- src/GameModel.java
+++ src/GameModel.java
@@ -13,16 +13,19 @@ import java.util.Random;
 public class GameModel {
 	private Player[] players;
 	private Deck sharedDeck;
+	private int gameProgress;
 	private int eventSuccess;
 	private int eventFail;
+	private int traitor;
 	
 	
 	public GameModel(String[] playerNames) {
 		Random random = new Random();
 		int numPlayers = playerNames.length;
-		int traitor = random.nextInt(numPlayers);
+		this.traitor = random.nextInt(numPlayers);
 		this.players = new Player[numPlayers];
 		this.sharedDeck = new Deck(numPlayers);
+		this.gameProgress = 5;
 		this.eventSuccess = 0;
 		this.eventFail = 0;
 		int i = 0;
@@ -36,9 +39,52 @@ public class GameModel {
 		}
 	}
 	
-	public boolean isGameOver() {
-		
-		return false;
+	/*
+	 * eliminates a player form the game
+	 * returns 0 if the player was eliminated successfully, -1 if the player was already eliminated
+	 */
+	public int eliminate(String name) {
+		for(Player p: players) {
+			if(p.getName().toLowerCase().equals(name)) {
+				if(!p.isAlive()) {
+					return -1;
+				}
+				p.eliminate();
+			}
+		}
+		return 0;
+	}
+	
+	/*
+	 * checks to see if any of the end-game conditions have been met
+	 * returns -1 if the game is not over, 0 if the group wins, 1 if the saboteur wins
+	 */
+	public int isGameOver() {
+		//checks to see if the saboteur is voted out
+		if (!players[traitor].isAlive()) {
+			return 0;
+		//checks to see if there are no more events to be played
+		}else if(gameProgress == 0){
+			//if more events where successful then the group wins
+			if (eventSuccess > eventFail){
+				return 0;
+			//if more events fail, or if there is a tie then the saboteur wins
+			}else {
+				return 1;
+			}
+		}else {
+			//checks to see if there are only two players left (sabotuer and a +1)
+			int playersAlive = 0;
+			for(Player p : players) {
+				if (p.isAlive()) {
+					playersAlive++;
+				}
+			}
+			if(playersAlive == 2) {
+				return 1;
+			}
+		}
+		return -1;
 	}
 	
 	public String toString() {
