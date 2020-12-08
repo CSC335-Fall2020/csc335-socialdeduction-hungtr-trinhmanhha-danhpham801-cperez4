d5ed025909ee1fd269ede6387bb93a9710ac1e18
Phamda1340 <58050461+Phamda1340@users.noreply.github.com>
2020-12-07 17:10:38 -0700

    Starter menu template

---
+++ src/StartMenu.java
@@ -0,0 +1,53 @@
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+public class StartMenu extends Application {
+	
+	 public void start(Stage s) {
+		 s.setTitle("Start Menu");
+		 TilePane root = new TilePane();
+		 Button start = new Button("Start");
+		 String str[] = { "Create New Server", "Join existing Sever"};
+		 
+		 start.setOnAction((event)-> {
+			 //send information to create instance of the game
+		 });
+		 
+		 HBox wrapper0 = new HBox();
+		 Label choice = new Label("Choose where to play: ");
+		 ChoiceBox cB = new ChoiceBox(FXCollections.observableArrayList(str));
+		 wrapper0.getChildren().addAll(choice, cB);
+		 
+		 HBox wrapper1 = new HBox();
+		 Label hNL = new Label("Host Name: ");
+		 TextField hostName = new TextField();
+		 wrapper1.getChildren().addAll(hNL, hostName);
+		 
+		 HBox wrapper2 = new HBox();
+		 Label pL = new Label("Port: ");
+		 TextField port = new TextField();
+		 wrapper2.getChildren().addAll(pL, port);
+		 
+		 root.getChildren().add(wrapper0);
+		 root.getChildren().add(wrapper1);
+		 root.getChildren().add(wrapper2);
+		 root.getChildren().add(start);
+	        
+		 Scene scene = new Scene(root, 252, 100);
+		 s.setScene(scene);
+		 s.show();
+	    }
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+
+}
