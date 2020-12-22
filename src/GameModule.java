c70e187bdcdad5c156a4dc74f84a96d153dd1d1e
Suah <pegasucksgg@gmail.com>
2020-12-22 15:37:08 -0700

    Modularize components

---
+++ src/GameModule.java
@@ -0,0 +1,21 @@
+/**
+ * @author Hung Tran
+ * <p>
+ * Created: Dec 22, 2020
+ * File: GameModule.java
+ * Desc:
+ */
+
+/**
+ * 
+ *
+ */
+public interface GameModule {
+	/**
+	 * Attaches rendering widgets into 'rendererPane'
+	 * @param rendererPane the main Pane that will be rendered
+	 * by the GameRenderer (or viewer)
+	 * @return a message type if necessary.
+	 */
+	public Object attachPane(Pane rendererPane);
+}
